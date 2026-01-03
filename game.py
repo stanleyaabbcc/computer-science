e4e397df2ac30d38ea63f3f75a33001c1df43b4d
stanley <stantu24@gmail.com>
2026-01-03 17:38:34 +0800

    1:31:00

--- game.py
+++ game.py
@@ -22,9 +22,12 @@ running = True
 
 #載入圖片
 player_img = pygame.image.load(os.path.join("img","player.png")).convert()
-rock_img = pygame.image.load(os.path.join("img","rock.png")).convert()
+#rock_img = pygame.image.load(os.path.join("img","rock.png")).convert()
 bullet_img = pygame.image.load(os.path.join("img","bullet.png")).convert()
-boss_img = pygame.image.load(os.path.join("img","boss.png")).convert()
+#boss_img = pygame.image.load(os.path.join("img","boss.png")).convert()
+rock_imgs = []
+for i  in range (5):
+    rock_imgs.append(pygame.image.load(os.path.join("img",f"rock{i}.png")).convert())
 
 
 class Player(pygame.sprite.Sprite):
@@ -56,7 +59,8 @@ class Player(pygame.sprite.Sprite):
 class Rock(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.image_ori = pygame.transform.scale(rock_img,(65,59))
+        #rock_imgs = pygame.transform.scale(rock_imgs,(65,59))
+        self.image_ori = random.choice(rock_imgs)
         #self.image = pygame.transform.scale(rock_img,(65,59))
         self.image = self.image_ori.copy()
         self.rect = self.image.get_rect() #定位物件
@@ -68,12 +72,15 @@ class Rock(pygame.sprite.Sprite):
         self.speedy = random.randrange(2, 10)
         self.speedx = random.randrange(-3, 3)
         self.total_degree = 0
-        self.rot_degree = 3
+        self.rot_degree = random.randrange(-3, 3)
 
     def rotate(self):
         self.total_degree += self.rot_degree
         self.total_degree = self.total_degree % 360
         self.image = pygame.transform.rotate(self.image_ori,self.total_degree)
+        center = self.rect.center
+        self.rect = self.image.get_rect()
+        self.rect.center = center
 
     def update(self):
         self.rotate()
